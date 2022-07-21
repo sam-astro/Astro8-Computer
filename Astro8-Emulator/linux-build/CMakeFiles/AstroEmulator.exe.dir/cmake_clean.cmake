6fd64cb2731fc812daabdb6c88fbb7931f106417
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 <caffenequery@gmail.com>
2022-07-21 10:43:10 -0400

    Add .exe to output file

---
+++ Astro8-Emulator/linux-build/CMakeFiles/AstroEmulator.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AstroEmulator.exe"
+  "AstroEmulator.exe.pdb"
+  "CMakeFiles/AstroEmulator.exe.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AstroEmulator.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
