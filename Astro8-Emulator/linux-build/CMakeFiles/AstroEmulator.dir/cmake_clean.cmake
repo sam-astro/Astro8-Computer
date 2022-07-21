5aa2093cd7b89df019c2fa80fd1dfd9da2760402
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 <caffenequery@gmail.com>
2022-07-21 10:19:46 -0400

    Configure CMake for cross platform builds

---
+++ Astro8-Emulator/linux-build/CMakeFiles/AstroEmulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AstroEmulator"
+  "AstroEmulator.pdb"
+  "CMakeFiles/AstroEmulator.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AstroEmulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
